b1648d2d1674fc860bb9890e5b82d498384aea16
KlausJager10110 <pacharapolpacharapol2547@gmail.com>
2026-02-10 16:22:17 +0700

    cell02 update

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -5,13 +5,11 @@ num2 = int(input("Enter the second number: "))
 
 result = num1 * num2
 
-print(num1, "X", num2, "=", result)
+print(num1, "x", num2, "=", result)
 
 if result > 0:
     print("The result is positive.")
 elif result < 0:
     print("The result is negative.")
 else:
-    print("The result is zero.")
-
-print("Result of multiplication:", result)
+    print("The result is positive and negative.")
